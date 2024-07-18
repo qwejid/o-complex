291220a53c73f3aa9b8e5b9864383a4fbb209995
Kristina Shishkina <klikklok2017@mail.ru>
2024-07-18 20:57:57 +0300

    Создание приложения

---
+++ app/weather/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import SearchHistory
+
+class WeatherAppTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+
+    def test_search_history(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.post('/', {'city': 'London'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(SearchHistory.objects.filter(user=self.user, city='London').exists())
