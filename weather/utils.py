ccdcb6a14c006e54537d28535c7084e6caf48c98
Kristina Shishkina <klikklok2017@mail.ru>
2024-07-19 19:03:00 +0300

    add исключения

--- weather/utils.py
+++ weather/utils.py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 import requests
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 
 def get_weather_data(lat, lon):
     params = {
@@ -10,12 +11,27 @@ def get_weather_data(lat, lon):
         'timezone': 'auto'  # Автоматическое определение временной зоны
     }
     url = "https://api.open-meteo.com/v1/forecast"
-    response = requests.get(url, params=params)
-    return response.json()
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Это выбросит исключение при ошибках HTTP
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        raise http_err
+    except requests.exceptions.RequestException as err:        
+        raise SystemExit(err) from err
+    
 
-def get_city_coordinates(city):
+def get_city_coordinates(city):  
     geolocator = Nominatim(user_agent="weather_app")
-    location = geolocator.geocode(city)
-    if location:
-        return location.latitude, location.longitude
-    return None, None
+    try:
+        location = geolocator.geocode(city)
+        if location:
+            return location.latitude, location.longitude
+        return None, None
+    except (GeocoderTimedOut, GeocoderServiceError) as e:        
+        print(f"Error while geocoding city '{city}': {e}")
+        return None, None
+    except Exception as e:        
+        print(f"Unexpected error: {e}")
+        return None, None
+
